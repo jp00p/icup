d4924dafeabc09ded7cd945e8d1118dda7193240
Jpop Wilson <asimovsghost@gmail.com>
2024-02-26 07:14:52 -0800

    weather

--- globals/scripts/globals.gd
+++ globals/scripts/globals.gd
@@ -1,11 +1,77 @@
 extends Node
 
 ## General
-var GAME_PAGES = {}
+var GAME_PAGES = {} # holds references to each page of the game
+
 enum RESOURCE_TYPES {
     HARVEST_ESSENCE=0
 }
 
+## Weather
+enum SEASONS {
+    SPRING,
+    SUMMER,
+    AUTUMN,
+    WINTER
+}
+
+enum WEATHER_CLOUDS {
+    CLEAR,
+    PARTLY_CLOUDY,
+    CLOUDY,
+    OVERCAST
+}
+
+enum WEATHER_PRECIP {
+    CLEAR,
+    RAIN,
+    SNOW
+}
+
+var PRECIP_CHANCES = {
+    SEASONS.SPRING: {
+        WEATHER_PRECIP.CLEAR: 0.6,
+        WEATHER_PRECIP.RAIN: 0.4
+    },
+    SEASONS.SUMMER: {
+        WEATHER_PRECIP.CLEAR: 0.9,
+        WEATHER_PRECIP.RAIN: 0.1,
+    },
+    SEASONS.AUTUMN: {
+        WEATHER_PRECIP.CLEAR: 0.3,
+        WEATHER_PRECIP.RAIN: 0.3,
+        WEATHER_PRECIP.SNOW: 0.3
+    },
+    SEASONS.WINTER: {
+        WEATHER_PRECIP.CLEAR: 0.25,
+        WEATHER_PRECIP.RAIN: 0.4,
+        WEATHER_PRECIP.SNOW: 0.4
+    }
+}
+
+var CLOUD_CHANCES = {
+    WEATHER_PRECIP.CLEAR: {
+        WEATHER_CLOUDS.CLEAR: 0.8,
+        WEATHER_CLOUDS.PARTLY_CLOUDY: 0.3,
+        WEATHER_CLOUDS.CLOUDY: 0.3
+    },
+    WEATHER_PRECIP.RAIN: {
+        WEATHER_CLOUDS.OVERCAST: 0.8,
+        WEATHER_CLOUDS.CLOUDY: 0.4,
+        WEATHER_CLOUDS.PARTLY_CLOUDY: 0.2
+    },
+    WEATHER_PRECIP.SNOW: {
+        WEATHER_CLOUDS.OVERCAST: 0.5,
+        WEATHER_CLOUDS.CLOUDY: 0.3,
+        WEATHER_CLOUDS.CLEAR: 0.1
+    }
+}
+
+func pick_weather(season:SEASONS=SEASONS.SPRING) -> Array:
+    var precip_type = WEATHER_PRECIP.values()[WeightedChoice.pick(PRECIP_CHANCES[season])]
+    var clouds_type = WEATHER_CLOUDS.values()[WeightedChoice.pick(CLOUD_CHANCES[precip_type])]
+    print("%s %s" % [precip_type, clouds_type])
+    return [ WEATHER_PRECIP.keys()[precip_type], WEATHER_CLOUDS.keys()[clouds_type] ]
 
 ## Gardening
 var BASE_PLANT = preload("res://ui/gardening/plant.tscn")
@@ -26,7 +92,7 @@ var PLANTS:Dictionary = {
 
 func _ready():
     var global_timer = Timer.new()
-    global_timer.wait_time = 0.1
+    global_timer.wait_time = 0.05
     global_timer.autostart = true
     global_timer.one_shot = false
     global_timer.connect("timeout", tick)
--- ui/scripts/weather.gd
+++ ui/scripts/weather.gd
@@ -1,19 +1,32 @@
 extends Control
 
+# measurements
+var hour_length : int = 5 # ticks
+var day_length : int = 24 # hours
+var season_length : int = 14 # days
+
+# timekeeping
 var local_tick : int = 0
+var season_tick : int = 0
 var global_tick : int = 0 : set = set_global_tick
 var ordinal : String = "AM"
 var time_of_day : int = 1
+var season : Globals.SEASONS = Globals.SEASONS.SPRING
 
+# sky objects
 var sunrise : int = 6
+var dusk : int = 15
 var moonrise : int = 19
 var starrise : int = 18
 var celestial_phase = "moonrise" : set = set_phase
 var celestial_progress : int = 0
 var stars_visible : bool = true
 var star_progress : int = 0
+var weather : Array
+var overcast : bool = false
+var raining : bool = false
 
-
+# all the nodes
 @onready var animations = %Animations
 @onready var sky_anim = %Sky
 @onready var sky_rect = %SkyRect
@@ -21,7 +34,10 @@ var star_progress : int = 0
 @onready var celestial_rect = %CelestialRect
 @onready var stars_rect = %StarsRect
 @onready var stars_anim = $Animations/Stars
-
+@onready var clouds_rect = %CloudsRect
+@onready var clouds_anim = %Clouds
+@onready var weather_anim = %Weather
+@onready var weather_rect = %WeatherRect
 
 func _ready():
     Signals.connect("global_tick", advance_time)
@@ -30,7 +46,7 @@ func _ready():
 
 func advance_time():
     local_tick += 1
-    if local_tick % 3 == 0:
+    if local_tick % hour_length == 0:
         local_tick = 0
         self.global_tick = global_tick + 1
 
@@ -47,9 +63,20 @@ func set_global_tick(tick):
     set_clock_text()
 
 func new_day():
+    set_weather()
+    season_tick += 1
+    if season_tick % season_length == 0:
+        next_season()
+
+func next_season():
+    pass
+
+func advance_clouds():
     pass
 
 func advance_celestial_objects():
+    celestial_rect.visible = !overcast
+    sky_rect.visible = !overcast
     if global_tick == sunrise:
         self.celestial_phase = "sun"
     if global_tick == moonrise:
@@ -59,12 +86,38 @@ func advance_celestial_objects():
         stars_visible = true
     if global_tick == (sunrise + 1):
         stars_visible = false
+    clouds_anim.frame = 0
+    if overcast and stars_visible:
+        clouds_anim.frame = 1
+
     celestial_progress += 1
     celestial_anim.frame = celestial_progress
-    stars_rect.visible = stars_visible
-    if stars_visible:
+    stars_rect.visible = stars_visible && !overcast
+    if stars_visible and !overcast:
         star_progress += 1
         stars_anim.frame = star_progress
+
+func set_weather():
+    weather = Globals.pick_weather(Globals.SEASONS.WINTER)
+    var precip = weather[0]
+    var clouds = weather[1]
+    print("%s %s" % [precip, clouds])
+    if precip == "CLEAR":
+        weather_rect.hide()
+        weather_anim.stop()
+        raining = false
+    else:
+        weather_rect.show()
+        weather_anim.animation = precip.to_lower()
+        weather_anim.play()
+        raining = true
+    if clouds == "CLEAR":
+        clouds_rect.hide()
+    else:
+        if clouds == "OVERCAST":
+            overcast = true
+        clouds_rect.show()
+        clouds_anim.animation = clouds.to_lower()
 
 func set_phase(phase):
     celestial_phase = phase
@@ -72,16 +125,31 @@ func set_phase(phase):
     celestial_progress = 0
 
 func set_clock_text():
-    %Label.text = "%s %s" % [time_of_day, ordinal]
+    %Label.text = "%s %s\n%s/%s" % [time_of_day, ordinal, weather[0].to_lower(), weather[1].to_lower()]
+
+func get_spriteframes_texture(frames:SpriteFrames, anim_name:String, frame:int) -> Texture2D:
+    return frames.get_frame_texture(anim_name, frame)
+
 
 func _on_sky_frame_changed():
-    var sky_texture = sky_anim.sprite_frames.get_frame_texture("daycycle", global_tick)
+    var sky_texture = get_spriteframes_texture(sky_anim.sprite_frames, "daycycle", global_tick)
     sky_rect.texture = sky_texture
 
 func _on_celestial_frame_changed():
-    var celes_texture = celestial_anim.sprite_frames.get_frame_texture(celestial_phase, celestial_progress)
+    var celes_texture = get_spriteframes_texture(celestial_anim.sprite_frames, celestial_phase, celestial_progress)
     celestial_rect.texture = celes_texture
 
 func _on_stars_frame_changed():
-    var stars_texture = stars_anim.sprite_frames.get_frame_texture("stars", star_progress)
+    var stars_texture = get_spriteframes_texture(stars_anim.sprite_frames, "stars", star_progress)
     stars_rect.texture = stars_texture
+
+func _on_weather_frame_changed():
+    var weather_texture = get_spriteframes_texture(weather_anim.sprite_frames, weather_anim.animation, weather_anim.frame)
+    weather_rect.texture = weather_texture
+
+func _on_clouds_frame_changed():
+    var clouds_texture = get_spriteframes_texture(clouds_anim.sprite_frames, clouds_anim.animation, clouds_anim.frame)
+    clouds_rect.texture = clouds_texture
+
+func _on_clouds_animation_changed():
+    _on_clouds_frame_changed()
